21c52846269c69cf1cbeaef6831f5e8c0a4605fc
Philip Ingrey <phil@ingrey.dev>
2021-10-31 20:59:13 +0000

    feat: &mut and far less copying

--- 2019-rust/day13-wasm/js/index.js
+++ 2019-rust/day13-wasm/js/index.js
@@ -10,14 +10,29 @@ function State(state, ctx, score) {
   this.ctx = ctx;
   this.size = 10;
   this.ended = false;
+  this.currentInput = 0;
   this.input = 0;
+  this.cheat = true;
+  this.hardMode = true;
+  this.paddlePosition = { x: 0, y: 0 };
+  this.ballPosition = { x: 0, y: 0 };
+  this.lastBallPosition = { x: -1, y: -1 };
 }
 State.prototype.step = function () {
-  this.state = this.state.step(this.input);
+  this.state.step(this.input);
   this.x = this.state.get_x();
   this.y = this.state.get_y();
   this.t = this.state.get_t();
   this.ended = this.state.is_ended();
+};
+State.prototype.debugRun = function () {
+  // run until the game ends:
+  while (true) {
+    this.step();
+    if (this.ended) {
+      return;
+    }
+  }
 };
 State.prototype.stepGame = function () {
   // draw until we draw the ball:
@@ -28,7 +43,10 @@ State.prototype.stepGame = function () {
     }
     if (this.x < 0) {
       // score
-      this.score.innerText = this.t;
+      if (this.t !== 0) {
+        // leave the final score up
+        this.score.innerText = this.t;
+      }
       continue;
     }
     const x = this.x * this.size;
@@ -53,6 +71,7 @@ State.prototype.stepGame = function () {
       case 3:
         this.ctx.fillStyle = "red";
         this.ctx.fillRect(x, y + w / 4, w, w / 2);
+        this.paddlePosition = { x: this.x, y: this.y };
         break;
       // ball
       case 4:
@@ -60,6 +79,42 @@ State.prototype.stepGame = function () {
         this.ctx.beginPath();
         this.ctx.ellipse(x + w / 2, y + w / 2, w / 2, w / 2, 0, 0, 2 * Math.PI);
         this.ctx.fill();
+        if (this.lastBallPosition.y == -1) {
+          // special bootstrap case
+          this.lastBallPosition = { x: this.x - 1, y: this.y - 1 };
+        } else {
+          this.lastBallPosition = this.ballPosition;
+        }
+        this.ballPosition = { x: this.x, y: this.y };
+        if (!this.cheat) {
+          this.input = this.currentInput;
+        } else {
+          // super-advanced-AI™️
+          if (this.ballPosition.y < this.lastBallPosition.y) {
+            // going up, play chase
+            if (this.ballPosition.x > this.paddlePosition.x) {
+              this.input = 1;
+            } else if (this.ballPosition.x < this.paddlePosition.x) {
+              this.input = -1;
+            } else {
+              this.input = 0;
+            }
+          } else {
+            // ball coming down, where is it going to land?
+            const dy = this.paddlePosition.y - 1 - this.ballPosition.y;
+            const dx =
+              this.ballPosition.x +
+              (this.ballPosition.x - this.lastBallPosition.x) * dy -
+              this.paddlePosition.x;
+            if (dx > 0) {
+              this.input = 1;
+            } else if (dx < 0) {
+              this.input = -1;
+            } else {
+              this.input = 0;
+            }
+          }
+        }
         // we return on a ball draw:
         return;
     }
@@ -71,20 +126,22 @@ State.prototype.runGame = function () {
   this.keydown = (e) => {
     switch (e.keyCode) {
       case 37:
-        that.input -= 1;
+        that.currentInput = -1;
+        that.input = -1;
         break;
       case 39:
-        that.input += 1;
+        that.currentInput = 1;
+        that.input = 1;
         break;
     }
   };
   this.keyup = (e) => {
     switch (e.keyCode) {
       case 37:
-        that.input += 1;
+        that.currentInput = 0;
         break;
       case 39:
-        that.input -= 1;
+        that.currentInput = 0;
         break;
     }
   };
@@ -94,24 +151,30 @@ State.prototype.runGame = function () {
   return new Promise((res, rej) => {
     // setup:
     that.stepGame();
-    that.interval = setInterval(() => {
-      if (that.ended) {
-        clearInterval(that.interval);
-        document.removeEventListener("keydown", that.keydown);
-        document.removeEventListener("keyup", that.keyup);
-        res();
-        return;
-      }
-      that.stepGame.call(that);
-    }, 250);
+    that.interval = setInterval(
+      () => {
+        if (that.ended) {
+          clearInterval(that.interval);
+          document.removeEventListener("keydown", that.keydown);
+          document.removeEventListener("keyup", that.keyup);
+          res();
+          return;
+        }
+        that.stepGame.call(that);
+      },
+      this.hardMode ? 20 : 250
+    );
   });
 };
 
 (async () => {
   const score = $("#score");
   const canvas = $("#canvas");
+  const cheatInput = $("#cheat");
+  const hardModeInput = $("#hardmode");
   const ctx = canvas.getContext("2d");
   const start = $("#start");
+  const debug = $("#debug");
   const rust = await import("../pkg/index.js").catch(console.error);
 
   // enable start button
@@ -120,7 +183,11 @@ State.prototype.runGame = function () {
   let pong;
   start.addEventListener("click", () => {
     start.setAttribute("disabled", "true");
-    pong = new State(rust.State.new_pong(), ctx, score);
+    pong = new State(rust.MutState.new_pong(), ctx, score);
+    pong.cheat = cheatInput.checked;
+    pong.hardMode = hardModeInput.checked;
+    score.innerText = "0";
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     pong
       .runGame()
@@ -128,4 +195,33 @@ State.prototype.runGame = function () {
       .then(() => start.removeAttribute("disabled"))
       .catch(console.log.bind(console));
   });
+
+  if (debug)
+    debug.addEventListener("click", () => {
+      debug.setAttribute("disabled", "true");
+      let initializationTime = 0;
+      let runTime = 0;
+      const globalStart = Date.now();
+      const N = 10;
+      let i = 0;
+      const oneRun = () => {
+        const start = performance.now();
+        const pong = new State(rust.MutState.new_pong(), ctx, score);
+        const initTime = performance.now() - start;
+        pong.debugRun();
+        runTime += performance.now() - initTime - start;
+        initializationTime += initTime;
+        score.innerText = `Completed ${i + 1}/10 in ${
+          Date.now() - globalStart
+        }ms total`;
+        if (++i == N) {
+          score.innerText = `${(10 * runTime) / N}ms`;
+          debug.removeAttribute("disabled");
+        } else {
+          // give it a breather:
+          setTimeout(oneRun, 100);
+        }
+      };
+      oneRun();
+    });
 })();
